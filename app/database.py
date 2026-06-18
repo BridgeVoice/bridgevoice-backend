15307bf89b5f583b7c49cb05ec3dfa76ff87ebd9
Juno James <junojamesv@gmail.com>
2026-06-18 14:28:55 -0400

    Migrate database connection to PostgreSQL

--- app/database.py
+++ app/database.py
@@ -2,13 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./bridgevoice.db"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./bridgevoice.db" 
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+SQLALCHEMY_DATABASE_URL = ("postgresql://postgres:bridgevoice123!@localhost:5432/bridgevoice")
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#engine = create_engine( SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, 
+                            autoflush=False, 
+                            bind=engine)
 
 Base = declarative_base()
 
