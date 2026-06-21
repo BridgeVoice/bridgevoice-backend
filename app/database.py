5565af06c3798e938d5ed16c08b1129a89980d1f
Juno James <junojamesv@gmail.com>
2026-06-20 23:52:00 -0400

    Add Supabase database configuration support

--- app/database.py
+++ app/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./bridgevoice.db" 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = ("postgresql://postgres:bridgevoice123!@localhost:5432/bridgevoice")
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./bridgevoice.db"
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 #engine = create_engine( SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
